274fd3c9166e1a3085a79543956d5e45c0dead59
Carlos <carcamfer@gmail.com>
2023-06-19 21:02:07 -0600

    Add input box

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hi there!');
+const fetchData = async () => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: 'abd9b957',
+        i: 'tt0848228'
+      }
+    });
+  
+    console.log(response.data);
+  };
+  
+  fetchData();
+  
