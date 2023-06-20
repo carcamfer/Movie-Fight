86120499a0b8fa8492cef2dcc43e22307cd1b2bf
Carlos <carcamfer@gmail.com>
2023-06-19 21:18:30 -0600

    Add eventhandler

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const fetchData = async () => {
+  const fetchData = async searchTerm => {
     const response = await axios.get('http://www.omdbapi.com/', {
       params: {
         apikey: 'abd9b957',
-        i: 'tt0848228'
+        s: searchTerm
       }
     });
   
     console.log(response.data);
   };
   
-  fetchData();
+  const input = document.querySelector('input');
+  input.addEventListener('input', event => {
+    fetchData(event.target.value);
+  });
   
