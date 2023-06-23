91363ca552050b98d9ccb4f8372e550f48fc03af
Carlos <carcamfer@gmail.com>
2023-06-22 20:14:31 -0600

    Followup Request

--- index.js
+++ index.js
@@ -47,6 +47,11 @@ const onInput = async event => {
       <img src="${imgSrc}" />
       ${movie.Title}
     `;
+    option.addEventListener('click', () => {
+      dropdown.classList.remove('is-active');
+      input.value = movie.Title;
+      onMovieSelect(movie);
+    });
 
     resultsWrapper.appendChild(option);
   }
@@ -58,3 +63,14 @@ document.addEventListener('click', event => {
     dropdown.classList.remove('is-active');
   }
 });
+
+const onMovieSelect = async movie => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'd9835cc5',
+      i: movie.imdbID
+    }
+  });
+
+  console.log(response.data);
+};
